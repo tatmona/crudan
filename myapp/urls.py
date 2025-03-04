6a71432685eb1639470d8b24d72feef9fa44d344
tatmona <tatbisimonank@gmail.com>
2025-03-04 13:18:47 +0300

    second commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,7 @@ from myapp import views
 urlpatterns = [
     path('', views.user_List),
     path('Add/', views.add_user),
-
+    path('Edit/<id>',views.Edituser),
+    path('Delete/<eid>',views.Deleteuser),
+    path('View/<eid>',views.Viewuser),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -13,8 +13,25 @@ def add_user(request):
         form.save()
         return redirect('/')
     mydict['form'] = form
-    return render(request,'add.html')
+    return render(request,'add.html',mydict)
 
+def Edituser(request,id=None):
+    one_rec=User.objects.get(pk=id)
+    form = UserForm(request.POST or None, request.FILES or None, instance=one_rec)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request,'edit.html',{'form':form})
 
+def Deleteuser(request,eid=None):
+    one_rec=User.objects.get(pk=eid)
+    if request.method=='POST':
+        one_rec.delete()
+        return redirect('/')
+    return render(request,'delete.html',{'form':one_rec})
 
-
+def Viewuser(request,eid=None):
+    mydict={}
+    one_rec=User.objects.get(pk=eid)
+    mydict['user']=one_rec
+    return render(request,'views.html', mydict)
